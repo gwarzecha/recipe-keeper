726216f2fa0c589e4bc9bd0077a545923593c23e
gwarz <gmwarzecha@tutanota.com>
2022-02-15 22:03:05 -0700

    add an option to change the color via the ThemeSelector component, using the useTheme hook

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Recipe from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
+import ThemeSelector from "./components/ThemeSelector";
 
 // styless
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
+      <ThemeSelector />
         <Switch>
           <Route exact path="/">
             <Home />
